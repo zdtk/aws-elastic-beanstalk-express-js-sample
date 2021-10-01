2a459f1a1c6aa6a95d53319eff0e00843abfae97
zdtk <zdtk9876@gmail.com>
2021-10-02 00:28:05 +0900

    pipeTest

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('TWO'));
+app.get('/', (req, res) => res.send('SANN'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
